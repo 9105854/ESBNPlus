c52b7d702122ae01eee341cc98d3d97d59cef3e4
9105854 <9105854@chac.qld.edu.au>
2024-02-10 11:25:17 +1000

    Added base template, half of nav

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,30 @@
 extern crate rocket;
 use std::time::Duration;
 
-use log;
-use rocket::response::Responder;
+use rocket::{
+    fs::FileServer,
+    response::{self, Responder},
+    Request,
+};
 use rocket_dyn_templates::{context, Template};
 use sqlx::{sqlite::SqlitePoolOptions, SqliteConnection, SqlitePool};
+#[derive(Debug)]
 struct AppError(anyhow::Error);
+impl<E> From<E> for AppError
+where
+    E: Into<anyhow::Error>,
+{
+    fn from(error: E) -> Self {
+        AppError(error.into())
+    }
+}
+
+impl<'r> Responder<'r, 'static> for AppError {
+    fn respond_to(self, request: &Request<'_>) -> response::Result<'static> {
+        response::Debug(self.0).respond_to(request)
+    }
+}
+
 #[get("/")]
 fn index() -> Template {
     Template::render("index", context![])
@@ -21,8 +40,10 @@ async fn rocket() -> _ {
         .connect("sqlite:data.db")
         .await
         .expect("Couldn't connect to db");
+    let assets_server = FileServer::from("assets");
     rocket::build()
         .mount("/", routes![index])
+        .mount("/assets", assets_server)
         .manage(pool)
         .attach(Template::fairing())
 }
